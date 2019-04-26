4c4341dde0538cde7a8c4b9a76494383dd58e3dd
Marc Davies <marcdaviesriot@gmail.com>
2019-04-26 08:38:45 -0700

    add initial commit

---
+++ WordCounter.Tests/ModelTests/WordTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+using System;
+using System.Collections.Generic;
+
+namespace WordCounter.Tests;
+{
+  [TestClass]
+  public class Word
+  {
+
+    // [TestMethod]
+    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+    //   // any necessary logic to prep for test; instantiating new classes, etc.
+    //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    // }
+
+  }
+}
